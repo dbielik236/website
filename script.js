d135393c9a8d8ad125c8a6d97017d40ea0cc705f
David Bielik <davidbielik@gmail.com>
2026-06-07 12:33:30 -0400

    seo updates

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-
-
-
-
-
-
-function updateDownloadAttribute() {
-  const download = document.getElementById("download-picture");
-
-  if (window.innerWidth < 1100) {
-    document.querySelectorAll("a > img.photos").forEach(img => {
-      const anchor = img.parentNode;
-      const column = anchor.parentNode;
-
-      column.insertBefore(img, anchor);
-      anchor.remove();
-    });
-
-    download.innerText = "";
-  }
-}
-
-// Run on page load
-updateDownloadAttribute();
-
-// Run on window resize
-window.addEventListener('resize', updateDownloadAttribute);
-
 window.onload = function () {
 
   emailjs.init("Abm0b8RIpGS0ezhMr");
@@ -74,51 +46,6 @@ function sendEmail(event) {
     });
 }
 
-function mailingList(event) {
-  event.preventDefault();
-
-  let form = document.getElementById("mailingList"); // Form ID here should match HTML
-  let button = form.querySelector("button"); // Get the button inside the form
-  
-
-  let templateParams = {
-      email: document.getElementById("mailing-list-email").value,
-  };
-
-  emailjs.send("service_gj83vl5", "template_n8j33kz", templateParams)
-      .then(response => {
-          // Change button text to "Success!" and disable it
-          button.textContent = "Success!";
-          button.disabled = true;
-
-          // Optional: clear the input field
-          document.getElementById("mailing-list-email").value = "";
-      })
-      
-}
-
-document.getElementById("copy-short-bio").addEventListener("click", function () {
-  const text = document.getElementById("short-bio-text").innerText;
-
-  navigator.clipboard.writeText(text).then(() => {
-    this.innerText = "Copied!";
-    setTimeout(() => {
-      this.innerText = "Copy bio";
-    }, 2000);
-  });
-});
-
-document.getElementById("copy-long-bio").addEventListener("click", function () {
-  const text = document.getElementById("long-bio-text").innerText;
-
-  navigator.clipboard.writeText(text).then(() => {
-    this.innerText = "Copied!";
-    setTimeout(() => {
-      this.innerText = "Copy bio";
-    }, 2000);
-  });
-});
-
 document.querySelectorAll('.dropdown-custom > .nav-link').forEach(link => {
   link.addEventListener('click', function(e) {
     if (window.innerWidth < 768) {
@@ -126,4 +53,10 @@ document.querySelectorAll('.dropdown-custom > .nav-link').forEach(link => {
       this.parentElement.classList.toggle('active');
     }
   });
+});
+
+const year = new Date().getFullYear();
+
+document.querySelectorAll(".year").forEach(element => {
+  element.textContent = year;
 });
